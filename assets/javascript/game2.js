759b9739c2a69d04ee7632e6082be1c91a9e6dbb
GregH <43968370+gfhorrocks@users.noreply.github.com>
2019-03-03 10:52:46 -0500

    New Thought Process

---
+++ assets/javascript/game2.js
@@ -0,0 +1,54 @@
+var game = {
+    playerName: ["Darth Maul", "Kylo Ren", "Qui Gon Jinn", "Yoda"],
+    healthPoints: [200, 150, 200, 100],
+    attackPower: [20, 15, 20, 15],
+    counterattackPower: [40, 30, 40, 30],
+    playerTracker: [1, 1, 1, 1],
+    enemyTracker: [1, 1, 1, 1],
+    imageLocation: ["./assets/images/darthmaulProfile.jpg",
+        "./assets/images/kylorenProfile.jpg",
+        "./assets/images/quigonjinnProfile.jpg",
+        "./assets/images/yodaProfile.jpeg"],
+
+    reset: function () {
+        this.healthPoints = [200, 150, 200, 100];
+        this.playerTracker = [1, 1, 1, 1];
+        this.enemyTracker = [1, 1, 1, 1];
+        this.defenderTracker = [1, 1, 1, 1];
+    },
+
+    drawCards: function (rw) {
+        for (i = 0; i < 4; i++) {
+
+            var playerCard = $("<div>");
+            var enemyCard = $("<div>");
+            var defenderCard = $("<div>");
+
+            enemyCard.addClass("enemyCard");
+            enemyCard.html("<h5>" + this.playerName[i] + "</h5> <img id='cardImage' src='" + this.imageLocation[i] + "'></br>");
+            enemyCard.append(this.healthPoints[i]);
+
+            playerCard.addClass("playerCard");
+            playerCard.html("<h5>" + this.playerName[i] + "</h5> <img id='cardImage' src='" + this.imageLocation[i] + "'></br>");
+            playerCard.append(this.healthPoints[i]);
+
+            defenderCard.addClass("defenderCard");
+            defenderCard.html("<h5>" + this.playerName[i] + "</h5> <img id='cardImage' src='" + this.imageLocation[i] + "'></br>");
+            defenderCard.append(this.healthPoints[i]);
+
+            if (game.playerTracker[i] === 1) {
+                $("#selectionRow").append(playerCard);
+            }
+            if (game.enemyTracker[i] === 1) {
+                $("#enemyRow").append(enemyCard);
+            }
+            if (game.defenderTracker[i] === 1) {
+                $("#defenderRow").append(defenderCard);
+            }
+        }
+    }
+};
+
+
+game.reset();
+game.drawCards();
